db6227b190d9a41c1aebfe430368e57b6699d925
MICHAELASDELL\Michaela <michaela.bang@telia.com>
2017-05-22 22:01:37 +0200

    Map Icon visible in IE

--- src/js/ie.js
+++ src/js/ie.js
@@ -4,6 +4,7 @@ $('.svg-cat').css('height', '100vh');
 $('.svg-cat').css('max-height', '100vh');
 $('.cat-logotype').css('width', '50%');
 $('.cat-logotype').css('height', '30%');
+$('.header').css('margin-right', '120px');
 
 //do stuff for IE.
 }
