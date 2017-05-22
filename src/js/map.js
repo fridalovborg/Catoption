6259ed363e87ea70e212e0cd38e7cb61b15dae5c
Anna Kilström <anna.kilstrom@bokningsbolaget.se>
2017-05-22 16:49:56 +0200

    ny sökväg igen i map, ändrat storlek på bilderna och ikoner i instaflödet..

--- src/js/map.js
+++ src/js/map.js
@@ -72,7 +72,7 @@ function addMarker(place) {
 		map: map,
 		animation: an,
 		position: place.geometry.location,
-		icon: '../img/icon.png'
+		icon: 'src/img/icon.png'
 	});
 
 	google.maps.event.addListener(marker, 'click', function() {
