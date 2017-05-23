aeaf33f81e082ae5e88d2f3653a5a321bbb392b1
Frida Lövborg <Frida@Fridas-MacBook-Pro.local>
2017-05-23 09:58:18 +0200

    database connection

--- src/js/db/input.php
+++ src/js/db/input.php
@@ -1,2 +1,6 @@
-<?php $db = new PDO('mysql:host=localhost;dbname=catoptionDB', 'root', '');
+<?php $db = new PDO('mysql:host=server5.serverdrift.com;dbname=annakils_catoption', 'annakils_cat', 'catoption2018');
 $db->exec('UPDATE theCounter SET value = (value + 1) WHERE id = 1');
+// server5.serverdrift.com = host
+// annakils_catoption = dbname
+// annakils_cat = användare
+// catoption2018 = lösen
--- src/js/db/output.php
+++ src/js/db/output.php
@@ -1,4 +1,4 @@
-<?php $db = new PDO('mysql:host=localhost;dbname=catoptionDB', 'root', '');
+<?php $db = new PDO('mysql:host=server5.serverdrift.com;dbname=annakils_catoption', 'annakils_cat', 'catoption2018');
 $patCat = $db
 	->query('SELECT value FROM theCounter WHERE id = 1')
 	->fetchAll(PDO::FETCH_ASSOC);
