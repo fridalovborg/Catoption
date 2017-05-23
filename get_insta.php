32360948087c6d778e554f659ccdf4b9a00e5155
Frida Lövborg <Frida@Fridas-MacBook-Pro.local>
2017-05-23 15:24:34 +0200

    comments and fix småsaker

--- get_insta.php
+++ get_insta.php
@@ -3,19 +3,19 @@
                 INSTAGRAM API
 ---------------------------------------------------------------------------- */
 $timeout = 1;
-$lastCache = file_get_contents("cache_time.txt");
+$lastCache = file_get_contents('cache_time.txt');
 
 if ($lastCache + $timeout < time() ) { // Har det gått <timeout> tid?
-    file_put_contents("cache_time.txt", time());
-    $url = "https://www.instagram.com/explore/tags/cats/?__a=1";
+    file_put_contents('cache_time.txt', time());
+    $url = 'https://www.instagram.com/explore/tags/cats/?__a=1';
     // @ framför undertrycker felmeddelanden
     @$data = file_get_contents($url);
     // Skriv bara om cache om API:et svarar. Annars behåll den gamla cachen.
     if ($data)
-        file_put_contents("cache.txt", $data);
+        file_put_contents('cache.txt', $data);
 }
 
-$data = json_decode(file_get_contents("cache.txt"));
+$data = json_decode(file_get_contents('cache.txt'));
 for ($i = 0; $i < 18; $i++) {
 	echo '<div class="img-cont">';
     echo '<img id="likes" src="'.$data->tag->media->nodes[$i]->thumbnail_src.'">';
--- src/js/main.js
+++ src/js/main.js
@@ -62,7 +62,7 @@ use this for updating the images of instagram page every minute.
 function getIGImages() {
 	// CREATING OBJECT
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "getIGdata.php?foo="+Math.random(), true);
+	xhr.open('GET', 'get_insta.php?foo='+Math.random(), true);
 
 	xhr.addEventListener('readystatechange', function(e) {
 		console.log(this);
@@ -94,7 +94,7 @@ var animTxt = TweenMax.to(audioTxt, 0.8, {scaleX: 1.2, scaleY: 1.2, repeat: -1, 
 // INTERVAL OF VOLUME ICON
 var icon = document.getElementById('icon');
 iconInterval = setInterval(function() {
-	$(icon).toggleClass("fa-volume-down fa-volume-up");
+	$(icon).toggleClass('fa-volume-down fa-volume-up');
 }, 800);
 
 setTimeout(function() {
@@ -120,9 +120,9 @@ function audioBtn() {
 
 	if(!player.paused) {
 		player.pause();
-		$(audioIcon).toggleClass("fa-volume-off fa-volume-up");
+		$(audioIcon).toggleClass('fa-volume-off fa-volume-up');
 	} else {
 		player.play();
-		$(audioIcon).toggleClass("fa-volume-up fa-volume-off");
+		$(audioIcon).toggleClass('fa-volume-up fa-volume-off');
 	}
 };
